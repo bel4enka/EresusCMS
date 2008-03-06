f2dc0c9aeb681c5f00a67aecded5d86f1131a30c
Михаил Красильников <mihalych@vsepofigu.ru>
2008-03-06 22:14:10 +0000

    0000093: Проверка совместимости с Eresus 2.10

--- ext/autoreplace.php
+++ ext/autoreplace.php
@@ -1,160 +1,77 @@
 <?php
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
-# AutoReplace (CMS Eresus™ Plugin)
-# © 2006, ProCreat Systems
-# Web: http://procreat.ru
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
-class TAutoReplace extends TListContentPlugin {
-  var
-    $name = 'autoreplace',
-    $title = 'Автозамена',
-    $type = 'client,admin',
-    $version = '0.02',
-    $description = 'Автозамена фрагментов страницы',
-    $settings = array(
-    );
-  var $table = array (
-    'name' => 'autoreplace',
-    'key'=> 'id',
-    'sortMode' => 'position',
-    'sortDesc' => false,
-    'columns' => array(
-      array('name' => 'caption', 'caption' => 'Замена'),
-    ),
-    'controls' => array (
-      'delete' => '',
-      'edit' => '',
-      'position' => '',
-      'toggle' => '',
-    ),
-    'tabs' => array(
-      'width'=>'180px',
-      'items'=>array(
-       array('caption'=>strAdd, 'name'=>'action', 'value'=>'create')
-      ),
-    ),
-    'sql' => "(
-      `id` int(10) unsigned NOT NULL auto_increment,
-      `active` tinyint(1) unsigned NOT NULL default '1',
-      `position` int(10) unsigned default NULL,
-      `caption` varchar(255) default '',
-      `src` varchar(255) default '',
-      `dst` varchar(255) default '',
-      `regexp` tinyint(1) default '0',
-      PRIMARY KEY  (`id`),
-      KEY `active` (`active`),
-      KEY `position` (`position`)
-    ) TYPE=MyISAM;",
-  );
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  # Стандартные функции
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  function TAutoReplace()
-  # производит регистрацию обработчиков событий
-  {
-  global $plugins;
+/**
+ * Call
+ *
+ * Eresus 2
+ *
+ * Вызов других плагинов посредством макросов.
+ *
+ * @version 2.00
+ *
+ * @copyright   2007, Eresus Group, http://eresus.ru/
+ * @license     http://www.gnu.org/licenses/gpl.txt  GPL License 3
+ * @maintainer  Михалыч <mk@procreat.ru>
+ * @author      Mikhail Krasilnikov <mk@procreat.ru>
+ *
+ * Данная программа является свободным программным обеспечением. Вы
+ * вправе распространять ее и/или модифицировать в соответствии с
+ * условиями версии 3 либо по вашему выбору с условиями более поздней
+ * версии Стандартной Общественной Лицензии GNU, опубликованной Free
+ * Software Foundation.
+ *
+ * Мы распространяем эту программу в надежде на то, что она будет вам
+ * полезной, однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том
+ * числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и ПРИГОДНОСТИ ДЛЯ
+ * ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ. Для получения более подробной
+ * информации ознакомьтесь со Стандартной Общественной Лицензией GNU.
+ *
+ */
 
-    parent::TPlugin();
-    $plugins->events['clientOnPageRender'][] = $this->name;
-    $plugins->events['adminOnMenuRender'][] = $this->name;
-  }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  # Внутренние функции
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  function insert()
+class Call extends Plugin {
+  var $version = '2.00a2';
+  var $kernel = '2.10b3';
+  var $title = 'Call';
+  var $description = 'Вызов плагинов из шаблонов';
+  var $type = 'client';
+	
+ /**
+  * Конструктор
+  *
+  * @return Call
+  */
+  function Call()
   {
-  global $db, $request;
-
-    $item = GetArgs($db->fields($this->table['name']), array('regexp'));
-    $db->insert($this->table['name'], $item);
-    goto($request['arg']['submitURL']);
+    parent::Plugin();
+    $this->listenEvents('clientOnPageRender');
   }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  function update()
-  {
-  global $db, $page, $request;
-
-    $item = $db->selectItem($this->table['name'], "`id`='".$request['arg']['update']."'");
-    $item = GetArgs($item, array('regexp'));
-    $item['src'] = AddSlashes($item['src']);
-    $db->updateItem($this->table['name'], $item, "`id`='".$request['arg']['update']."'");
-    goto($request['arg']['submitURL']);
-  }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  # Административные функции
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  function adminAddItem()
-  {
-  global $page, $request;
-
-    $form = array(
-      'name' => 'AddForm',
-      'caption' => 'Добавить автозамену',
-      'width'=>'100%',
-      'fields' => array (
-        array ('type' => 'hidden', 'name' => 'action', 'value' => 'insert'),
-        array ('type' => 'edit', 'name' => 'caption', 'label' => 'Название', 'width' => '100%', 'maxlength' => '255'),
-        array ('type' => 'edit', 'name' => 'src', 'label' => 'Что заменять', 'width' => '100%', 'maxlength' => '255', 'pattern' => '/.+/', 'errormsg' => 'Вы должны указать текст в поле "Что заменять"'),
-        array ('type' => 'checkbox', 'name' => 'regexp', 'label' => 'Регулярное выражение'),
-        array ('type' => 'edit', 'name' => 'dst', 'label' => 'На что заменять', 'width' => '100%', 'maxlength' => '255'),
-      ),
-      'buttons' => array('ok', 'cancel'),
-    );
-
-    $result = $page->renderForm($form);
-    return $result;
-  }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  function adminEditItem()
-  {
-  global $db, $page, $request;
-
-    $item = $db->selectItem($this->table['name'], "`id`='".$request['arg']['id']."'");
-    $form = array(
-      'name' => 'EditForm',
-      'caption' => 'Редактировать автозамену',
-      'width' => '500px',
-      'fields' => array (
-        array('type'=>'hidden','name'=>'update', 'value'=>$item['id']),
-        array ('type' => 'edit', 'name' => 'caption', 'label' => 'Название', 'width' => '100%', 'maxlength' => '255'),
-        array ('type' => 'edit', 'name' => 'src', 'label' => 'Что заменять', 'width' => '100%', 'maxlength' => '255', 'pattern' => '/.+/', 'errormsg' => 'Вы должны указать текст в поле "Что заменять"'),
-        array ('type' => 'checkbox', 'name' => 'regexp', 'label' => 'Регулярное выражение'),
-        array ('type' => 'edit', 'name' => 'dst', 'label' => 'На что заменять', 'width' => '100%', 'maxlength' => '255'),
-      ),
-      'buttons' => array('ok', 'apply', 'cancel'),
-    );
-    $result = $page->renderForm($form, $item);
-    return $result;
-  }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  function adminRender()
-  {
-    return $this->adminRenderContent();
-  }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  # Обработчики событий
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
+  //-----------------------------------------------------------------------------
+ /**
+  * Обработчик события clientOnPageRender
+  *
+  * @param string $text
+  * @return string
+  */
   function clientOnPageRender($text)
   {
-    global $db;
-
-    $items = $db->select($this->table['name'], '`active`=1', $this->table['sortMode'], $this->table['sortDesc']);
-    if (count($items)) foreach ($items as $item) {
-      if ($item['regexp'])
-        $text = preg_replace(StripSlashes($item['src']), $item['dst'], $text);
-      else
-        $text = str_replace(StripSlashes($item['src']), $item['dst'], $text);
+    global $Eresus;
+    
+    preg_match_all('/\$\(call:(.*)(::(.*)({(.*)})?)?\)/Usi', $text, $calls, PREG_SET_ORDER);
+    foreach($calls as $call) {
+    	$name = strtolower($call[1]);
+    	$method = count($call) > 3 ? strtolower($call[3]) : null;
+      if (isset($Eresus->plugins->list[$name])) {
+        $plugin = isset($Eresus->plugins->items[$name]) ? $Eresus->plugins->items[$name] : $Eresus->plugins->load($name);
+        if ($method) {
+	        if (method_exists($plugin, $method)) {
+	        	$args = count($call) > 5 ? $call[5] : null;
+	          $result = call_user_func(array($plugin, $method), $args);
+	          if (is_string($result)) $text = str_replace($call[0], $result, $text);
+	        } else ErrorMessage("Method '$method' not found in plugin '$name'");
+        }
+      } else ErrorMessage("Plugin '$name' not installed or disabled");
     }
     return $text;
   }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-  function adminOnMenuRender()
-  {
-  global $page;
-
-    $page->addMenuItem('Расширения', array ("access"  => EDITOR, "link"  => $this->name, "caption"  => $this->title, "hint"  => $this->description));
-  }
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
+  //-----------------------------------------------------------------------------
 }
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 ?>
