ac75cf5f8c052f037ca4a818875ea88a5f91bb10
Михаил Красильников <mihalych@vsepofigu.ru>
2008-05-15 08:17:58 +0000

    [*] 0000152: нужно изменить основной дамп движка - необходимо убрать NULL в default

--- distrib/mysql.sql
+++ distrib/mysql.sql
@@ -4,34 +4,34 @@
 --
 
 CREATE TABLE `pages` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `name` varchar(32) NOT NULL default '',
-  `owner` int(10) unsigned NOT NULL default '0',
-  `title` text default NULL,
-  `caption` varchar(64) default NULL,
-  `description` text default NULL,
-  `hint` text default NULL,
-  `keywords` text default NULL,
-  `position` smallint(5) unsigned default NULL,
-  `active` tinyint(1) NOT NULL default '0',
-  `access` tinyint(1) unsigned default NULL,
-  `visible` tinyint(1) unsigned default NULL,
-  `template` varchar(64) default NULL,
-  `type` varchar(32) NOT NULL default 'default',
-  `content` longtext,
-  `options` text,
-  `created` datetime default NULL,
-  `updated` datetime default NULL,
-  PRIMARY KEY  (`id`),
-  KEY `name` (`name`),
-  KEY `owner` (`owner`),
-  KEY `position` (`position`),
-  KEY `active` (`active`),
-  KEY `access` (`access`),
-  KEY `visible` (`visible`),
-  KEY `created` (`created`),
-  KEY `updated` (`updated`)
-) TYPE=MyISAM COMMENT='Структура и страницы сайта';
+	`id` int(10) unsigned NOT NULL auto_increment,
+	`name` varchar(32) NOT NULL default '',
+	`owner` int(10) unsigned NOT NULL default '0',
+	`title` text NOT NULL default '',
+	`caption` varchar(64) NOT NULL default '',
+	`description` text NOT NULL default '',
+	`hint` text NOT NULL default '',
+	`keywords` text NOT NULL default '',
+	`position` smallint(5) unsigned NOT NULL default '0',
+	`active` tinyint(1) NOT NULL default '0',
+	`access` tinyint(1) unsigned NOT NULL default 5,
+	`visible` tinyint(1) unsigned NOT NULL default '1',
+	`template` varchar(64) NOT NULL default '',
+	`type` varchar(32) NOT NULL default 'default',
+	`content` longtext NOT NULL default '',
+	`options` text NOT NULL default '',
+	`created` datetime NOT NULL default '0000-00-00 00:00:00',
+	`updated` datetime NOT NULL default '0000-00-00 00:00:00',
+	PRIMARY KEY  (`id`),
+	KEY `name` (`name`),
+	KEY `owner` (`owner`),
+	KEY `position` (`position`),
+	KEY `active` (`active`),
+	KEY `access` (`access`),
+	KEY `visible` (`visible`),
+	KEY `created` (`created`),
+	KEY `updated` (`updated`)
+) TYPE=MyISAM;
 
 --
 -- Дамп данных таблицы `pages`
@@ -46,18 +46,18 @@ INSERT INTO `pages` VALUES (1, 'main', 0, 'Главная', 'Главная', '', 'Главная стр
 --
 
 CREATE TABLE `plugins` (
-  `name` varchar(32) NOT NULL default '',
-  `type` set('client','admin','content','ondemand') default NULL,
-  `active` tinyint(1) unsigned NOT NULL default '1',
-  `position` int(10) unsigned default NULL,
-  `settings` text,
-  `title` varchar(64) default NULL,
-  `version` varchar(16) default NULL,
-  `description` varchar(255) default NULL,
-  PRIMARY KEY  (`name`),
-  KEY `active` (`active`),
-  KEY `position` (`position`),
-  KEY `type` (`type`)
+	`name` varchar(32) NOT NULL default '',
+	`type` set('client','admin','content','ondemand') default NULL,
+	`active` tinyint(1) unsigned NOT NULL default '1',
+	`position` int(10) unsigned default NULL,
+	`settings` text,
+	`title` varchar(64) default NULL,
+	`version` varchar(16) default NULL,
+	`description` varchar(255) default NULL,
+	PRIMARY KEY  (`name`),
+	KEY `active` (`active`),
+	KEY `position` (`position`),
+	KEY `type` (`type`)
 ) TYPE=MyISAM COMMENT='Модули расширения';
 
 --
@@ -72,20 +72,20 @@ CREATE TABLE `plugins` (
 --
 
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `login` varchar(16) NOT NULL default '',
-  `hash` varchar(32) NOT NULL default '',
-  `active` tinyint(1) unsigned NOT NULL default '1',
-  `lastVisit` datetime default NULL,
-  `lastLoginTime` int(10) unsigned default NULL,
-  `loginErrors` int(10) unsigned default NULL,
-  `access` tinyint(3) unsigned default NULL,
-  `name` varchar(64) default NULL,
-  `mail` varchar(64) default NULL,
-  `profile` text,
-  PRIMARY KEY  (`id`),
-  KEY `login` (`login`),
-  KEY `active` (`active`)
+	`id` int(10) unsigned NOT NULL auto_increment,
+	`login` varchar(16) NOT NULL default '',
+	`hash` varchar(32) NOT NULL default '',
+	`active` tinyint(1) unsigned NOT NULL default '1',
+	`lastVisit` datetime default NULL,
+	`lastLoginTime` int(10) unsigned default NULL,
+	`loginErrors` int(10) unsigned default NULL,
+	`access` tinyint(3) unsigned default NULL,
+	`name` varchar(64) default NULL,
+	`mail` varchar(64) default NULL,
+	`profile` text,
+	PRIMARY KEY  (`id`),
+	KEY `login` (`login`),
+	KEY `active` (`active`)
 ) TYPE=MyISAM PACK_KEYS=0 COMMENT='Пользователи';
 
 --
