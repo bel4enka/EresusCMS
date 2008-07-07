36d20a51435d7b2b7c81864835469dafff0740a0
Михаил Красильников <mihalych@vsepofigu.ru>
2008-07-07 08:45:06 +0000

    [*] Обновлены unit-тесты.

--- t/Framework/init.php
+++ t/Framework/init.php
@@ -1,10 +1,13 @@
 <?php
 	require_once('lib.php');
 
+	overwrite('main.php', '/cfg/');
+	overwrite('settings.inc', '/cfg/');
+	overwrite('test_db.php', '/core/lib/');
+
 	require_test('../core/kernel.php');
 	require_once('Eresus.class.php');
 
-	require_test('../core/kernel.php');
 	require_test('../core/lib/sections.php');
 	require_test('../core/classes.php');
 	require_test('../core/client.php');
--- t/Framework/lib.php
+++ t/Framework/lib.php
@@ -12,9 +12,19 @@ function require_test($filename)
 		$included[] = $filename;
 		$code = file_get_contents($filename);
 		$code = preg_replace('/^\s*###cut:start\s.*###cut:end.*$/Ums', '', $code);
+		if (!is_dir(dirname($tempname))) mkdir(dirname($tempname), 0777, true);
 		file_put_contents($tempname, $code);
 		require($tempname);
 	}
+}
+//-----------------------------------------------------------------------------
+function overwrite($file, $path)
+{
+	$target = dirname(__FILE__).'/php'.$path;
+	@mkdir($target, 0777, true);
+	$target .= $file;
+	@unlink($target);
+	copy(dirname(__FILE__).'/overwrite/'.$file, $target);
 }
 //-----------------------------------------------------------------------------
 
@@ -23,6 +33,7 @@ class EresusFatalException extends Exception {}
 
 function FatalError($message)
 {
+	$message = iconv('cp1251', 'utf8', $message);
 	throw new EresusFatalException(strip_tags($message));
 }
 //-----------------------------------------------------------------------------
