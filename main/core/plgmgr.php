f4714348c26edc7c4f7d0b8de78f4a666fffa1ed
Михаил Красильников <mihalych@vsepofigu.ru>
2010-06-11 07:49:03 +0000

    Переделан диалог добавления плагинов

--- main/core/plgmgr.php
+++ main/core/plgmgr.php
@@ -40,10 +40,9 @@ class TPlgMgr
 	 * Уровень доступа к модулю
 	 * @var int
 	 */
-	public $access = ADMIN;
+	private $access = ADMIN;
 
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function toggle()
+	private function toggle()
 	{
 		global $page, $Eresus;
 
@@ -51,8 +50,8 @@ class TPlgMgr
 		$item = $Eresus->db->selectItem('plugins', "`name`='".$Eresus->request['arg']['toggle']."'");
 		HTTP::redirect($page->url());
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function delete()
+
+	private function delete()
 	{
 	global $page, $Eresus;
 
@@ -60,8 +59,8 @@ class TPlgMgr
 		$Eresus->plugins->uninstall($Eresus->request['arg']['delete']);
 		HTTP::redirect($page->url());
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function edit()
+
+	private function edit()
 	{
 	global $page, $Eresus;
 
@@ -82,8 +81,8 @@ class TPlgMgr
 		}
 		return $result;
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function update()
+
+	private function update()
 	{
 	global $page, $Eresus;
 
@@ -91,93 +90,140 @@ class TPlgMgr
 		$Eresus->plugins->items[$Eresus->request['arg']['update']]->updateSettings();
 		HTTP::redirect($Eresus->request['arg']['submitURL']);
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function insert()
+
+	/**
+	 * Подключает плагины
+	 *
+	 * @return void
+	 * @see add()
+	 */
+	private function insert()
 	{
 		global $page, $Eresus;
 
 		$files = arg('files');
 		if ($files && is_array($files))
+		{
 			foreach ($files as $plugin => $install)
+			{
 				if ($install)
 					$Eresus->plugins->install($plugin);
+			}
 
-		HTTP::redirect(arg('submitURL'));
+		}
+		HttpResponse::redirect('admin.php?mod=plgmgr');
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function add()
+	//-----------------------------------------------------------------------------
+
+	/**
+	 * Возвращает диалог добавления плагина
+	 *
+	 * @return string  HTML
+	 */
+	private function add()
 	{
 		global $page, $Eresus;
 
+		$data = array();
+
+		/*
+		 * Составляем список доступных плагинов
+		 */
+		$files = glob($Eresus->froot . 'ext/*.php');
+		if ($files === false)
+		{
+			$files = array();
+		}
+
+		/*
+		 * Составляем список установленных плагинов
+		 */
 		$items = $Eresus->db->select('`plugins`', '', "`name`");
 		$installed = array();
-		for ($i = 0; $i < count($items); $i++) $installed[] = filesRoot.'ext/'.$items[$i]['name'].'.php';
+		foreach ($items as $item)
+		{
+			$installed []= $Eresus->froot . 'ext/' . $item['name'] . '.php';
+		}
 
-		$files = glob(filesRoot.'ext/*.php');
-		if ($files === false)
-			$files = array();
+		// Оставляем только неустановленные
 		$files = array_diff($files, $installed);
 
-		$page->scripts .= '
-			function checkboxes(type)
+		/*
+		 * Собираем информацию о неустановленных плагинах
+		 */
+		$data['plugins'] = array();
+		if (count($files))
+		{
+			foreach ($files as $file)
 			{
-				var temp = document.forms.FoundPlugins;
-				var inp = temp.getElementsByTagName("input");
-				var i = 0;
-				while (i < inp.length)
+				$plugin = array('errors' => array());
+				// Считываем исходник
+				$s = file_get_contents($file);
+				// Имя плагина должно совпадать с именем файла
+				$plugin['name'] = basename($file, '.php');
+				// Если нет класса "ИмяПлагина" или "TИмяПлагина" (старая форма) - это не файла плагина
+				if (preg_match('/class\s+T?' . $plugin['name'] . '\s.*?{(.*?)({|})/is',	$s, $s))
 				{
-					if (inp[i].type == "checkbox")
+					// $s теперь содержит исходник плагина
+					$s = $s[1];
+
+					/* Ищем нужные свойства */
+					preg_match('/\$kernel\s*=\s*(\'|")(.+)\1/', $s, $kernel);
+					preg_match('/\$version\s*=\s*(\'|")(.+)\1/', $s, $version);
+					preg_match('/\$title\s*=\s*(\'|")(.+)\1/', $s, $title);
+					preg_match('/\$description\s*=\s*(\'|")(.+)\1/', $s, $description);
+
+					// FIXME: Совместимость с версиями до 2.10b2. Надо сделать проверку на наличие $kernel
+					if (count($version) && count($title) && count($description))
 					{
-						if (type)
-							inp[i].setAttribute("checked", "checked");
-						else
-							inp[i].removeAttribute("checked");
+						$plugin['title'] = $title[2];
+						$plugin['version'] = $version[2];
+						$plugin['description'] = $description[2];
 					}
-					i++;
+					else
+					{
+						$invalid = admPluginsNotRequiredFields;
+					}
+
+					/* PHP < 5.3 не понимает "rc", только "RC", но остальные буквы должны быть только
+					 * в нижнем регистре
+					 */
+					if (isset($kernel[2]))
+					{
+						$plugin['kernel'] =  str_replace('rc','RC', $kernel[2]);
+					}
+					else
+					{
+						$plugin['kernel'] =  str_replace('rc','RC', $kernel);
+					}
+
+					$kernelVersion = str_replace('rc','RC', CMSVERSION);
+					if (
+						count($kernel) &&
+						version_compare($v_plugin, $v_kernel, '>')
+					)
+					{
+						$msg =  I18n::getInstance()->getText('admPluginsInvalidVersion', $this);
+						$plugin['errors'] []= sprintf($msg, $plugin['kernel']);
+					}
 				}
-				return false;
+				else
+				{
+					$msg =  I18n::getInstance()->getText('Class "%s" not found in plugin file', $this);
+					$plugin['errors'] []= sprintf($msg, $plugin['name']);
+				}
+				$data['plugins'] []= $plugin;
 			}
-		';
-
-		$form = array(
-			'name' => 'FoundPlugins',
-			'caption' => admPluginsFound,
-			'width' => '600px',
-			'buttons' => array('ok','cancel'),
-			'fields' => array(
-				array('type'=>'hidden','name'=>'action','value'=>'insert'),
-				array('type'=>'text','value'=>'Выбрать: [<a href="#" onclick="return checkboxes(true);">Все</a>]  [<a href="#" onclick="return checkboxes(false);">Ни одного</a>]'),
-			),
-		);
-		if (count($files)) foreach($files as $file) {
-			$s = file_get_contents($file);
-			$name = basename($file, '.php'); # Имя плагина
-			$invalid = !preg_match('/class\s+T?'.$name.'\s.*?{(.*?)({|})/is', $s, $s);
-			if (!$invalid) {
-				$s = $s[1];
-				preg_match('/\$kernel\s*=\s*(\'|")(.+)\1/', $s, $kernel);
-				preg_match('/\$version\s*=\s*(\'|")(.+)\1/', $s, $version);
-				preg_match('/\$title\s*=\s*(\'|")(.+)\1/', $s, $title);
-				preg_match('/\$description\s*=\s*(\'|")(.+)\1/', $s, $description);
-				#FIXME: Совместимость с версиями до 2.10b2. Надо проверять и наличие $kernel
-				if (count($version) && count($title) && count($description)) {
-					$caption = "{$title[2]} {$version[2]} - {$description[2]}";
-				} else $invalid = admPluginsNotRequiredFields;
-				# PHP < 5.3 does not understatnd lowercase 'rc' but other letters must be only lowercase
-				if (isset($kernel[2])) $v_plugin =  str_replace('rc','RC', $kernel[2]);
-				else $v_plugin =  str_replace('rc','RC', $kernel);
-
-				$v_kernel =  str_replace('rc','RC', CMSVERSION);
-				if (count($kernel) && version_compare($v_plugin, $v_kernel, '>')) $invalid = sprintf(admPluginsInvalidVersion, $kernel[2]);
-			} else $invalid = admPluginsInvalidFile;
-			if ($invalid) $caption = '<span class="admError">'.$name.' - '.$invalid.'</span>';
-			$form['fields'][] = array('type'=>'checkbox','name'=>'files['.$name.']','label'=>$caption, 'value'=>true, 'disabled'=>$invalid);
 		}
-		$result = $page->renderForm($form);
-		return $result;
+
+		$tmpl = $page->getUITheme()->getTemplate('PluginManager/add-dialog.html');
+		$html = $tmpl->compile($data);
+
+		return $html;
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function up()
+	//-----------------------------------------------------------------------------
+
+	private function up()
 	{
 		global $page, $Eresus;
 
@@ -189,8 +235,8 @@ class TPlgMgr
 		}
 		HTTP::redirect($page->url());
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
-	function down()
+
+	private function down()
 	{
 		global $page, $Eresus;
 
@@ -202,14 +248,13 @@ class TPlgMgr
 		}
 		HTTP::redirect($page->url());
 	}
-	#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 	/**
 	 * Отрисовка контента модуля
 	 *
 	 * @return string
 	 */
-	function adminRender()
+	public function adminRender()
 	{
 		global $page, $Eresus;
 
@@ -218,39 +263,66 @@ class TPlgMgr
 
 		$result = '';
 		$page->title = admPlugins;
-		if (isset($Eresus->request['arg']['update'])) $this->update();
-		elseif (isset($Eresus->request['arg']['toggle'])) $this->toggle();
-		elseif (isset($Eresus->request['arg']['delete'])) $this->delete();
-		elseif (isset($Eresus->request['arg']['id'])) $result = $this->edit();
-		elseif (isset($Eresus->request['arg']['up'])) $this->up();
-		elseif (isset($Eresus->request['arg']['down'])) $this->down();
-		elseif (isset($Eresus->request['arg']['action'])) switch($Eresus->request['arg']['action']) {
-			case 'add': $result = $this->add(); break;
-			case 'insert': $this->insert(); break;
-		} else {
-			$table = array (
-				'name' => 'plugins',
-				'key' => 'name',
-				'sortMode' => 'position',
-				'columns' => array(
-					array('name' => 'title', 'caption' => admPlugin, 'width' => '90px', 'wrap'=>false),
-					array('name' => 'description', 'caption' => admDescription),
-					array('name' => 'version', 'caption' => admVersion, 'width'=>'70px','align'=>'center'),
-				),
-				'controls' => array (
-					'delete' => '',
-					'edit' => '',
-					'toggle' => '',
-					'position' => ''
-				),
-				'tabs' => array(
-					'width'=>'180px',
-					'items'=>array(
-						array('caption'=>admPluginsAdd, 'name'=>'action', 'value'=>'add')
+
+		switch (true)
+		{
+			case arg('update') !== null:
+				$this->update();
+			break;
+
+			case arg('toggle') !== null:
+				$this->toggle();
+			break;
+
+			case arg('delete') !== null:
+				$this->delete();
+			break;
+
+			case arg('id') !== null:
+				$result = $this->edit();
+			break;
+
+			case arg('up') !== null:
+				$this->up();
+			break;
+
+			case arg('down') !== null:
+				$this->down();
+			break;
+
+			case arg('action') == 'add':
+				$result = $this->add();
+			break;
+
+			case arg('action') == 'insert':
+				$this->insert();
+			break;
+
+			default:
+				$table = array (
+					'name' => 'plugins',
+					'key' => 'name',
+					'sortMode' => 'position',
+					'columns' => array(
+						array('name' => 'title', 'caption' => admPlugin, 'width' => '90px', 'wrap'=>false),
+						array('name' => 'description', 'caption' => admDescription),
+						array('name' => 'version', 'caption' => admVersion, 'width'=>'70px','align'=>'center'),
+					),
+					'controls' => array (
+						'delete' => '',
+						'edit' => '',
+						'toggle' => '',
+						'position' => ''
+					),
+					'tabs' => array(
+						'width'=>'180px',
+						'items'=>array(
+							array('caption'=>admPluginsAdd, 'name'=>'action', 'value'=>'add')
+						)
 					)
-				)
-			);
-			$result = $page->renderTable($table);
+				);
+				$result = $page->renderTable($table);
+			break;
 		}
 		return $result;
 	}
