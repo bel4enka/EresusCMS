e9576c182317e2c018c1b1354a5ad1365b984975
Михаил Красильников <mihalych@vsepofigu.ru>
2010-11-24 21:46:01 +0000

    548: ORM Doctrine URL: http://bugs.eresus.ru/view.php?id=548
    Eresus::reset_login переделан через ORM

--- main/core/kernel-legacy.php
+++ main/core/kernel-legacy.php
@@ -1318,19 +1318,20 @@ class Eresus
 		$this->user['auth'] = isset($this->user['auth']) ? $this->user['auth'] : false;
 		if ($this->user['auth'])
 		{
-			$item = $this->db->selectItem('users', "`id`='".$this->user['id']."'");
-			if (!is_null($item))
+			$item = Doctrine_Core::getTable('User')->find($this->user['id']);
+			//$item = $this->db->selectItem('users', "`id`='".$this->user['id']."'");
+			if ($item)
 			{ # Если такой пользователь есть...
-				if ($item['active'])
+				if ($item->active)
 				{ # Если учетная запись активна...
-					$this->user['name'] = $item['name'];
-					$this->user['mail'] = $item['mail'];
-					$this->user['access'] = $item['access'];
-					$this->user['profile'] = decodeOptions($item['profile']);
+					$this->user['name'] = $item->name;
+					$this->user['mail'] = $item->mail;
+					$this->user['access'] = $item->access;
+					$this->user['profile'] = decodeOptions($item->profile);
 				}
 				else
 				{
-					ErrorMessage(sprintf(errAccountNotActive, $item['login']));
+					ErrorMessage(sprintf(errAccountNotActive, $item->login));
 					$this->logout();
 				}
 			}
