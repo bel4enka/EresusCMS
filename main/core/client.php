80a05f92af2783f4fc7528c26d8bd27588aaba48
Михаил Красильников <mihalych@vsepofigu.ru>
2011-05-24 05:44:18 +0000

    Удалены морально устаревшие методы и свойства

--- main/core/client.php
+++ main/core/client.php
@@ -30,23 +30,12 @@
  */
 
 /**
- * Признак клиентского интерфейса
- *
- * @var bool
- */
-define('CLIENTUI', true);
-
-/**
  * Страница клиентского интерфейса
  *
  * @package UI
  */
 class TClientUI extends WebPage
 {
-	//var $section = array(); # Массив заголовков страниц
-	//var $Document; # DOM-интерфейс к странице
-	//var $plugin; # Плагин контента
-
 	/**
 	 * Текущий раздел сайта
 	 *
@@ -69,100 +58,6 @@ class TClientUI extends WebPage
 	 */
 	public $templateName;
 
-	/**
-	 * Подставляет значения макросов
-	 *
-	 * @param unknown_type $text
-	 *
-	 * @return void
-	 *
-	 * @since ?.??
-	 */
-	function replaceMacros($text)
-	{
-		global $Eresus;
-
-		/*$section = $this->section;
-		if (siteTitleReverse) $section = array_reverse($section);
-		$section = strip_tags(implode($section, option('siteTitleDivider')));*/
-
-		$result = str_replace(
-			array(
-				'$(httpHost)',
-				'$(httpPath)',
-				'$(httpRoot)',
-				'$(styleRoot)',
-				'$(dataRoot)',
-
-				'$(siteName)',
-				'$(siteTitle)',
-				'$(siteKeywords)',
-				'$(siteDescription)',
-
-				'$(pageId)',
-				'$(pageName)',
-				'$(pageTitle)',
-				'$(pageCaption)',
-				'$(pageHint)',
-				'$(pageDescription)',
-				'$(pageKeywords)',
-				'$(pageAccessLevel)',
-				//'$(pageAccessName)',
-
-				//'$(sectionTitle)',
-			),
-			array(
-				httpHost,
-				httpPath,
-				httpRoot,
-				styleRoot,
-				dataRoot,
-
-				siteName,
-				siteTitle,
-				siteKeywords,
-				siteDescription,
-
-				$this->id,
-				$this->name,
-				$this->title,
-				$this->caption,
-				$this->hint,
-				$this->description,
-				$this->keywords,
-				$this->access,
-				//constant('ACCESSLEVEL'.$this->access),
-				//$section,
-			),
-			$text
-		);
-		//$result = preg_replace_callback('/\$\(const:(.*?)\)/i', '__macroConst', $result);
-		//$result = preg_replace_callback('/\$\(var:(([\w]*)(\[.*?\]){0,1})\)/i', '__macroVar', $result);
-		$result = preg_replace('/\$\(\w+(:.*?)*?\)/', '', $result);
-		return $result;
-	}
-	//------------------------------------------------------------------------------
- /**
-	* Отрисовка переключателя страниц
-	*
-	* @param int     $total      Общее количество страниц
-	* @param int     $current    Номер текущей страницы
-	* @param string  $url        Шаблон адреса для перехода к подстранице.
-	* @param array   $templates  Шаблоны оформления
-	* @return string
-	*/
-	function pageSelector($total, $current, $url = null, $templates = null)
-	{
-		if (is_null($url)) $url = $this->url().'p%d/';
-		useLib('templates');
-		$Templates = new Templates();
-		$defaults = explode('---', $Templates->get('PageSelector', 'std'));
-		if (!is_array($templates)) $templates = array();
-		for ($i=0; $i < 5; $i++) if (!isset($templates[$i])) $templates[$i] = $defaults[$i];
-		$result = parent::pageSelector($total, $current, $url, $templates);
-		return $result;
-	}
-	//------------------------------------------------------------------------------
 	/**
 	 * Производит разбор URL и загрузку соответствующего раздела
 	 *
@@ -248,36 +143,6 @@ class TClientUI extends WebPage
 		return $result; */
 	}
 
-	/**
-	 * Проводит инициализацию интерфейса
-	 *
-	 * @return void
-	 *
-	 * @since ?.??
-	 */
-	public function init()
-	{
-		$event = new Eresus_CMS_Event('clientOnStart');
-		$event->dispath();
-
-		$this->section = $this->loadPage();
-		if ($this->section)
-		{
-			/*if (count($Eresus->request['params']))
-			{
-				if (preg_match('/p[\d]+/i', $Eresus->request['params'][0]))
-					$this->subpage = substr(array_shift($Eresus->request['params']), 1);
-
-				if (count($Eresus->request['params']))
-					$this->topic = array_shift($Eresus->request['params']);
-			}*/
-		}
-		else
-		{
-			$this->httpError(404);
-		}
-	}
-	//------------------------------------------------------------------------------
 	function httpError($code)
 	{
 	global $KERNEL;
@@ -326,7 +191,6 @@ class TClientUI extends WebPage
 			$this->template = 'default';
 			$this->content = '<h1>HTTP ERROR '.$code.': '.$message.'</h1>';
 		}
-		$KERNEL['ERROR'] = true;
 		$this->render();
 		exit;
 	}
@@ -341,6 +205,26 @@ class TClientUI extends WebPage
 	 */
 	public function render()
 	{
+		$event = new Eresus_CMS_Event('clientOnStart');
+		$event->dispath();
+
+		$this->section = $this->loadPage();
+		if ($this->section)
+		{
+			/*if (count($Eresus->request['params']))
+			{
+				if (preg_match('/p[\d]+/i', $Eresus->request['params'][0]))
+					$this->subpage = substr(array_shift($Eresus->request['params']), 1);
+
+				if (count($Eresus->request['params']))
+					$this->topic = array_shift($Eresus->request['params']);
+			}*/
+		}
+		else
+		{
+			$this->httpError(404);
+		}
+
 		// Выбираем шаблон для страницы
 		$this->templateName = $this->section->template;
 
@@ -401,177 +285,6 @@ class TClientUI extends WebPage
 		//if (!$Eresus->conf['debug']['enable']) ob_start('ob_gzhandler');
 		echo $html;
 		//if (!$Eresus->conf['debug']['enable']) ob_end_flush();
-	}
-	//------------------------------------------------------------------------------
-	function pages($pagesCount, $itemsPerPage, $reverse = false)
-	# Выводит список подстраниц для навигации по ним
-	{
-	global $Eresus;
-
-		if ($pagesCount>1) {
-			$at_once = option('clientPagesAtOnce');
-			if (!$at_once) $at_once = 10;
-
-			$side_left = '';
-			$side_right = '';
-
-			$for_from = $reverse ? $pagesCount : 1;
-			$default = $for_from;
-			$for_to = $reverse ? 0 : $pagesCount+1;
-			$for_delta = $reverse ? -1 : 1;
-
-			# Если количество страниц превышает AT_ONCE
-			if ($pagesCount > $at_once) {
-				if ($reverse) { # Если установлен обратный порядок страниц
-					if ($this->subpage < ($pagesCount - (integer)($at_once / 2))) $for_from = ($this->subpage + (integer)($at_once / 2));
-					if ($this->subpage < (integer)($at_once / 2)) $for_from = $at_once;
-					$for_to = $for_from - $at_once;
-					if ($for_to < 0) {$for_from += abs($for_to); $for_to = 0;}
-					if ($for_from != $pagesCount) $side_left = "<a href=\"".$Eresus->request['path']."\" title=\"".strLastPage."\">&nbsp;&laquo;&nbsp;</a>";
-					if ($for_to != 0) $side_right = "<a href=\"".$Eresus->request['path']."p1/\" title=\"".strFirstPage."\">&nbsp;&raquo;&nbsp;</a>";
-				} else { # Если установлен прямой порядок страниц
-					if ($this->subpage > (integer)($at_once / 2)) $for_from = $this->subpage - (integer)($at_once / 2);
-					if ($pagesCount - $this->subpage < (integer)($at_once / 2) + (($at_once % 2)>0)) $for_from = $pagesCount - $at_once+1;
-					$for_to = $for_from + $at_once;
-					if ($for_from != 1) $side_left = "<a href=\"".$Eresus->request['path']."\" title=\"".strFirstPage."\">&nbsp;&laquo;&nbsp;</a>";
-					if ($for_to < $pagesCount) $side_right = "<a href=\"".$Eresus->request['path']."p".$pagesCount."/\" title=\"".strLastPage."\">&nbsp;&raquo;&nbsp;</a>";
-				}
-			}
-			$result = '<div class="pages">'.strPages;
-			$result .= $side_left;
-			for ($i = $for_from; $i != $for_to; $i += $for_delta)
-				if ($i == $this->subpage) $result .= '<span class="selected">&nbsp;'.$i.'&nbsp;</span>';
-					else $result .= '<a href="'.$Eresus->request['path'].($i==$default?'':'p'.$i.'/').'">&nbsp;'.$i.'&nbsp;</a>';
-			$result .= $side_right;
-			$result .= "</div>\n";
-			return $result;
-		}
-	}
-	//------------------------------------------------------------------------------
-	function renderForm($form, $values=null)
-	{
-		global $Eresus;
-
-		$result = '';
-		$hidden = '';
-		$body = '';
-		$validator = '';
-		$html = false;
-		$file = false;
-		if (empty($form['name'])) ErrorMessage(errFormHasNoName);
-		if (count($form['fields'])) foreach($form['fields'] as $item) {
-			if ((!isset($item['access'])) || (UserRights($item['access']))) {
-				if (isset($item['label'])) $label = !empty($item['hint']) ? '<span class="hint" title="'.$item['hint'].'">'.$item['label'].'</span>': $item['label']; else $label = '';
-				if (isset($item['pattern'])) $validator .= "if (!form.".$item['name'].".value.match(".$item['pattern'].")) {\nalert('".(empty($item['errormsg'])?sprintf(errFormPatternError, $item['name'], $item['pattern']):$item['errormsg'])."');\nresult = false;\nform.".$item['name'].".select();\n} else ";
-				$value =
-					isset($item['value'])
-						? $item['value']
-						: (isset($item['name']) && isset($values[$item['name']])
-								? $values[$item['name']]
-								: (isset($item['default'])
-										? $item['default']
-										: ''
-									)
-							);
-				$width = isset($item['width'])?' style="width: '.$item['width'].';"':'';
-				$disabled = isset($item['disabled']) && $item['disabled']?' disabled':'';
-				$extra = isset($item['extra'])?' '.$item['extra']:'';
-				$comment = isset($item['comment'])?' '.$item['comment']:'';
-				switch(strtolower($item['type'])) {
-					case 'hidden':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$hidden .= '<input type="hidden" name="'.$item['name'].'" value="'.$value.'" />'."\n";
-					break;
-					case 'divider': $body .= "<tr><td colspan=\"2\"><hr></td></tr>\n"; break;
-					case 'text': $body .= '<tr><td colspan="2" class="formText"'.$extra.'>'.$value."</td></tr>\n"; break;
-					case 'header': $body .= '<tr><th colspan="2" class="formHeader">'.$value."</th></tr>\n"; break;
-					case 'edit':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$body .= '<tr><td class="formLabel">'.$label.'</td><td><input type="text" name="'.$item['name'].'" value="'.EncodeHTML($value).'"'.(empty($item['maxlength'])?'':' maxlength="'.$item['maxlength'].'"').$width.$disabled.$extra.' />'.$comment."</td></tr>\n"; break;
-					break;
-					case 'password':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$body .= '<tr><td class="formLabel">'.$label.'</td><td><input type="password" name="'.$item['name'].'"'.(empty($item['maxlength'])?'':' maxlength="'.$item['maxlength']).'"'.$width.$extra.' />'.$comment."</td></tr>\n";
-						if (isset($item['equal'])) $validator .= "if (form.".$item['name'].".value != form.".$item['equal'].".value) {\nalert('".errFormBadConfirm."');\nresult = false;\nform.".$item['name'].".value = '';\nform.".$item['equal'].".value = ''\nform.".$item['equal'].".select();\n} else ";
-					break;
-					case 'select':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$body .= '<tr><td class="formLabel">'.$label.'</td><td><select name="'.$item['name'].'"'.$width.$disabled.$extra.'>'."\n";
-						if (!isset($item['items']) && isset($item['values'])) $item['items'] = $item['values'];
-						for($i = 0; $i < count($item['items']); $i++) {
-							if (isset($item['values'])) $value = $item['values'][$i]; else $value = $i;
-							$body .= '<option value="'.$value.'" '.($value == (isset($values[$item['name']]) ? $values[$item['name']] : (isset($item['value'])?$item['value']:'')) ? 'selected="selected"' : '').">".$item['items'][$i]."</option>\n";
-						}
-						$body .= '</select>'.$comment."</td></tr>\n";
-					break;
-					case 'listbox':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$body .= '<tr><td class="formLabel">'.$label.'</td><td><select multiple="multiple" name="'.$item['name'].'[]"'.$width.(isset($item['height'])?' size="'.$item['height'].'"':'').$disabled.$extra.">\n";
-						if (!isset($item['items']) && isset($item['values'])) $item['items'] = $item['values'];
-						for($i = 0; $i< count($item['items']); $i++) {
-							if (isset($item['values'])) $value = $item['values'][$i]; else $value = $i;
-							$body .= '<option value="'.$value.'" '.(count($values) && in_array($value, $values[$item['name']]) ? 'selected="selected"' : '').">".$item['items'][$i]."</option>\n";
-						}
-						$body .= '</select>'.$comment."</td></tr>\n";
-					break;
-					case 'checkbox':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$body .= '<tr><td>&nbsp;</td><td><input type="checkbox" name="'.$item['name'].'" value="'.($value ? $value : true).'" '.($value ? 'checked="checked"' : '').$disabled.$extra.' style="background-color: transparent; border-style: none; margin:0px;" /><span style="vertical-align: baseline"> '.$label."</span></td></tr>\n";
-					break;
-					case 'memo':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$body .= '<tr><td colspan="2">'.(empty($label)?'':'<span class="formLabel">'.$label.'</span><br />').'<textarea name="'.$item['name'].'" cols="40" rows="'.(empty($item['height'])?'1':$item['height']).'" '.$width.$disabled.$extra.' >'.EncodeHTML($value)."</textarea></td></tr>\n";
-					break;
-					case 'file':
-						if (empty($item['name'])) ErrorMessage(sprintf(errFormFieldHasNoName, $item['type'], $form['name']));
-						$body .= '<tr><td class="formLabel">'.$label.'</td><td><input type="file" name="'.$item['name'].'" size="'.$item['width'].'"'.$disabled." />".$comment."</td></tr>\n";
-						$file = true;
-					break;
-					default: ErrorMessage(sprintf(errFormUnknownType, $item['type'], $form['name']));
-				}
-			}
-		}
-		$this->addScripts("
-			function ".$form['name']."Submit()
-			{
-				var result = true;
-				var form = document.forms.namedItem('".$form['name']."');
-				".(empty($validator)?'':$validator)."
-				if (result) {
-					var controls = form.elements;
-					var count = controls.length;
-					for (var i=0; i < count; i++) if (controls[i].type == 'checkbox') {
-						var control = document.createElement('input');
-						control.type = 'hidden';
-						control.name = controls[i].name;
-						control.value = controls[i].checked?controls[i].value:0;
-						controls[i].name = '';
-						form.appendChild(control);
-					}
-				}
-				return result;
-			}
-		", 'defer');
-		#if (!empty($validator)) $this->scripts .= "function ".$form['name']."Submit(strForm)\n{\nvar result = true;\n".$validator.";\nreturn result;\n}\n\n";
-		$result .=
-			"<div style=\"width: ".$form['width']."\" class=\"form\">\n".
-			"<form ".(empty($form['name'])?'':'id="'.$form['name'].'" ')."action=\"".(empty($form['action'])?$Eresus->request['path']:$form['action'])."\" method=\"post\"".(empty($validator)?'':' onsubmit="return '.$form['name'].'Submit();"').($file?' enctype="multipart/form-data"':'').">\n".
-			"<div class=\"hidden\"><input type=\"hidden\" name=\"submitURL\" value=\"".$this->url()."\" />".
-			$hidden."</div>\n".
-			"<table>\n".
-			(empty($form['caption'])?'':"<tr><th colspan=\"2\">".$form['caption']."</th></tr>\n").
-			"<colgroup><col width=\"0*\" /><col width=\"100%\" /></colgroup>\n".
-			$body.
-			"<tr><td colspan=\"2\" class=\"buttons\"><br />".
-			(in_array('ok', $form['buttons'])?'<input type="submit" class="button" value="OK" /> ':'').
-			(array_key_exists('ok', $form['buttons'])?'<input type="submit" class="button" value="'.$form['buttons']['ok'].'" /> ':'').
-			(in_array('reset', $form['buttons'])?'<input type="reset" class="button" value="'.strReset.'" /> ':'').
-			(array_key_exists('reset', $form['buttons'])?'<input type="reset" class="button" value="'.$form['buttons']['reset'].'" /> ':'').
-			(in_array('cancel', $form['buttons'])?'<input type="button" class="button" value="'.strCancel.'" onclick="javascript:history.back();" />':'').
-			(array_key_exists('cancel', $form['buttons'])?'<input type="button" class="button" value="'.$form['buttons']['cancel'].'" onclick="javascript:history.back();" />':'').
-			"</td></tr>\n".
-			"</table>\n</form></div>\n";
-		return $result;
 	}
 	//------------------------------------------------------------------------------
 }
