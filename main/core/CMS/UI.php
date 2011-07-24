d414c39203a19e097a0d5613380d4cde3f19efbb
Михаил Красильников <mihalych@vsepofigu.ru>
2011-07-24 19:34:09 +0000

    Добавлен метод Eresus_CMS_UI::getInstance()

--- main/core/CMS/UI.php
+++ main/core/CMS/UI.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * ${product.title} ${product.version}
- *
- * ${product.description}
+ * ${product.title}
  *
  * Абстрактный интерфейс CMS
  *
- * @copyright 2011, Eresus Project, http://eresus.ru/
+ * @version ${product.version}
+ * @copyright ${product.copyright}
  * @license ${license.uri} ${license.name}
- * @author Mikhail Krasilnikov <mihalych@vsepofigu.ru>
+ * @author Михаил Красильников <mihalych@vsepofigu.ru>
  *
  * Данная программа является свободным программным обеспечением. Вы
  * вправе распространять ее и/или модифицировать в соответствии с
@@ -26,7 +25,7 @@
  * GNU с этой программой. Если Вы ее не получили, смотрите документ на
  * <http://www.gnu.org/licenses/>
  *
- * @package CMS
+ * @package Eresus
  *
  * $Id$
  */
@@ -34,11 +33,48 @@
 /**
  * Абстрактный интерфейс CMS
  *
- * @package CMS
+ * @package Eresus
  * @since 2.16
  */
 abstract class Eresus_CMS_UI
 {
+	/**
+	 * Экземпляр-одиночка
+	 *
+	 * @var Eresus_CMS_UI
+	 */
+	private static $instance;
+
+	/**
+	 * Возвращает, при необходимости создавая, объект интерфейса
+	 *
+	 * @param string $className имя класса, потомка Eresus_CMS_UI
+	 *
+	 * @throws LogicException  если аргумент $className опущен и экземпляр Eresus_CMS_UI не был создан
+	 *                         ранее или если $className не потомок Eresus_CMS_UI
+	 * @return Eresus_CMS_UI
+	 *
+	 * @since 2.16
+	 */
+	public static function getInstance($className = null)
+	{
+		if (!self::$instance)
+		{
+			if (!$className)
+			{
+				throw new LogicException('No instance of Eresus_CMS_UI exists and no class name given');
+			}
+			$instance = new $className;
+			if (!($instance instanceof self))
+			{
+				throw new LogicException('Given class is not a descendant of Eresus_CMS_UI');
+			}
+			self::$instance = $instance;
+		}
+		return self::$instance;
+	}
+	//-----------------------------------------------------------------------------
+
 	/**
 	 * Обрабатывает запрос и возвращает ответ
 	 *
@@ -47,20 +83,5 @@ abstract class Eresus_CMS_UI
 	 * @since 2.16
 	 */
 	abstract public function process();
-	//-----------------------------------------------------------------------------
-
-	/**
-	 * Возвращает глобальный объект CMS
-	 *
-	 * @param string $name
-	 *
-	 * @return object
-	 *
-	 * @since 2.16
-	 */
-	protected function get($name)
-	{
-		return Eresus_Kernel::app()->get($name);
-	}
 	//-----------------------------------------------------------------------------
 }
--- tests/phpunit/core/CMS/AllTests.php
+++ tests/phpunit/core/CMS/AllTests.php
@@ -34,6 +34,7 @@ PHP_CodeCoverage_Filter::getInstance()->addFileToBlacklist(__FILE__);
 require_once dirname(__FILE__) . '/ContentPlugin_Test.php';
 require_once dirname(__FILE__) . '/Request_Test.php';
 require_once dirname(__FILE__) . '/Plugin_Test.php';
+require_once dirname(__FILE__) . '/UI_Test.php';
 
 class Eresus_CMS_AllTests
 {
@@ -44,6 +45,7 @@ class Eresus_CMS_AllTests
 		$suite->addTestSuite('Eresus_CMS_ContentPlugin_Test');
 		$suite->addTestSuite('Eresus_CMS_Plugin_Test');
 		$suite->addTestSuite('Eresus_CMS_Request_Test');
+		$suite->addTestSuite('Eresus_CMS_UI_Test');
 
 		return $suite;
 	}
