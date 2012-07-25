d3b51bc437c777c66ec0679183649f38dc3ccd1a
Илья Розанов <rozanov@dvaslona.ru>
2012-07-25 11:08:25 +0000

    Добавлен тест для setColumn класса AdminList.

---
+++ tests/phpunit/core/lib/admin/AdminList_Test.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * ${product.title}
+ *
+ * @version ${product.version}
+ *
+ * PhpUnit Tests
+ *
+ * @copyright 2010, Eresus Project, http://eresus.ru/
+ * @license ${license.uri} ${license.name}
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Eresus_CMS
+ * @subpackage Tests
+ * @author Михаил Красильников <mk@eresus.ru>
+ *
+ * $Id: CMS_Test.php 2187 2012-05-24 17:07:44Z mk $
+ */
+
+require_once dirname(__FILE__) . '/../../../bootstrap.php';
+require_once TESTS_SRC_DIR . '/core/lib/admin/lists.php';
+
+/**
+ * @package Eresus_CMS
+ * @subpackage Tests
+ */
+class Eresus_AdminList_Test extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @covers AdminList::setColumn
+	 */
+	public function test_setColumn()
+	{
+		 $AdminList = new AdminList();
+		 $AdminList->columns = array(0=>array(1, 2));
+     $AdminList->setColumn(1, array(3, 4));      
+     $this->assertEquals(array(array(1, 2), array(3, 4)),$AdminList->columns);
+		
+		 $AdminList->setColumn(0, array(3, 4));      
+     $this->assertEquals(array(array(1, 2, 3, 4), array(3, 4)),$AdminList->columns);
+           
+	}
+	
+	/* */
+}
