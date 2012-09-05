45b089309abfa219d2546ac7e7d5d4f458cd4053
Михаил Красильников <mihalych@vsepofigu.ru>
2012-09-05 18:03:06 +0400

    Доработан скрипт setperms.sh

--- src/bin/setperms.sh
+++ src/bin/setperms.sh
@@ -5,8 +5,36 @@
 # @author Михаил Красильников <mihalych@vsepofigu.ru>
 
 home=`dirname $0`
-chmod a+rw "$home/../cfg/settings.php"
-chmod -R a+rw "$home/../data"
-chmod -R a+rw "$home/../style"
-chmod -R a+rw "$home/../templates"
-chmod -R a+rw "$home/../var"
+
+function doChmod
+{
+  if ! chmod $1 $2
+  then
+    echo "Can't chmod $2!"
+    exit 1
+  fi
+}
+
+function makeWritable
+{
+  if [ -f $1 ]
+  then
+    doChmod 'a+rw' $1
+  else
+    if [ -d $1 ]
+    then
+      doChmod 'a+rw -R' $1
+    else
+      echo "ERROR: File $1 not exist!"
+      exit 2
+    fi
+  fi
+}
+
+makeWritable "$home/../cfg/settings.php"
+makeWritable "$home/../data"
+makeWritable "$home/../style"
+makeWritable "$home/../templates"
+makeWritable "$home/../var"
+
+echo "All done!"
