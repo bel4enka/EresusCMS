fad4ea45c582760738dcde2590beccff6025720a
Михаил Красильников <mihalych@vsepofigu.ru>
2012-09-07 23:25:03 +0400

    Класс EresusExtensions вынесен в отдельный файл.

--- src/core/Extensions/VendorRegistry.php
+++ src/core/Extensions/VendorRegistry.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * ${product.title} ${product.version}
+ * ${product.title}
  *
- * ${product.description}
+ * Класс для работы с расширениями системы
  *
- * @copyright 2004, Михаил Красильников <mihalych@vsepofigu.ru>
- * @copyright 2007, Eresus Project, http://eresus.ru/
+ * @version ${product.version}
+ * @copyright ${product.copyright}
  * @license ${license.uri} ${license.name}
  * @author Михаил Красильников <mihalych@vsepofigu.ru>
  *
@@ -26,12 +26,7 @@
  * <http://www.gnu.org/licenses/>
  *
  * @package Eresus
- *
- * $Id$
  */
-
-
-
 
 
 /**
@@ -41,21 +36,22 @@
  */
 class EresusExtensions
 {
- /**
-	* Загруженные расширения
-	*
-	* @var array
-	*/
-	var $items = array();
- /**
-	* Определение имени расширения
-	*
-	* @param string $class     Класс расширения
-	* @param string $function  Расширяемая функция
-	* @param string $name      Имя расширения
-	*
-	* @return mixed  Имя расширения или false если подходящего расширения не найдено
-	*/
+	/**
+	 * Загруженные расширения
+	 *
+	 * @var array
+	 */
+	private $items = array();
+
+	/**
+	 * Определение имени расширения
+	 *
+	 * @param string $class     Класс расширения
+	 * @param string $function  Расширяемая функция
+	 * @param string $name      Имя расширения
+	 *
+	 * @return mixed  Имя расширения или false если подходящего расширения не найдено
+	 */
 	function get_name($class, $function, $name = null)
 	{
 		$result = false;
@@ -74,16 +70,16 @@ class EresusExtensions
 
 		return $result;
 	}
-	//-----------------------------------------------------------------------------
- /**
-	* Загрузка расширения
-	*
-	* @param string $class     Класс расширения
-	* @param string $function  Расширяемая функция
-	* @param string $name      Имя расширения
-	*
-	* @return mixed  Экземпляр класса Eresus_Extensions_Connector или false если не удалось загрузить расширение
-	*/
+
+	/**
+	 * Загрузка расширения
+	 *
+	 * @param string $class     Класс расширения
+	 * @param string $function  Расширяемая функция
+	 * @param string $name      Имя расширения
+	 *
+	 * @return mixed  Экземпляр класса Eresus_Extensions_Connector или false
+	 */
 	function load($class, $function, $name = null)
 	{
 		$result = false;
@@ -93,12 +89,13 @@ class EresusExtensions
 		{
 			$result = $this->items[$name];
 		}
-			else
+		else
 		{
 			$filename = Eresus_CMS::getLegacyKernel()->froot.'ext-3rd/' . $name .
 				'/eresus-connector.php';
 			if (is_file($filename))
 			{
+				/** @noinspection PhpIncludeInspection */
 				include_once $filename;
 				$class = $name.'Connector';
 				if (class_exists($class))
@@ -110,5 +107,4 @@ class EresusExtensions
 		}
 		return $result;
 	}
-	//-----------------------------------------------------------------------------
 }
