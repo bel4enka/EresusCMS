45dc697c03304fa0a35fc17e723a1aca84ce877c
Михаил Красильников <mihalych@vsepofigu.ru>
2012-09-08 22:28:18 +0400

    Упрощён Eresus_HTTP_Request. За счёт этого удалён метод Eresus_CMS::detectWebRoot.

--- tests/phpunit/core/Eresus/CMSTest.php
+++ tests/phpunit/core/Eresus/CMSTest.php
@@ -38,81 +38,9 @@ require_once __DIR__ . '/../../bootstrap.php';
 class Eresus_CMSTest extends PHPUnit_Framework_TestCase
 {
 	/**
-	 * @covers Eresus_CMS::detectWebRoot
-	 */
-	public function test_detectWebRoot()
-	{
-		/* Подменяем DOCUMENT_ROOT */
-		$webServer = Eresus_WebServer::getInstance();
-		$documentRoot = new ReflectionProperty('Eresus_WebServer', 'documentRoot');
-		$documentRoot->setAccessible(true);
-		$documentRoot->setValue($webServer, '/home/user/public_html');
-
-		$cms = $this->getMock('Eresus_CMS', array('getFsRoot'));
-		$cms->expects($this->any())->method('getFsRoot')
-			->will($this->returnValue('/home/user/public_html'));
-
-		$request = $this->getMock('UniversalStub', array('setLocalRoot'));
-		$request->expects($this->once())->method('setLocalRoot')->with('');
-		Eresus_Kernel::sc()->set('request', $request);
-
-		$detectWebRoot = new ReflectionMethod('Eresus_CMS', 'detectWebRoot');
-		$detectWebRoot->setAccessible(true);
-		$detectWebRoot->invoke($cms);
-	}
-
-	/**
-	 * @covers Eresus_CMS::detectWebRoot
-	 */
-	public function test_detectWebRoot_notRoot()
-	{
-		/* Подменяем DOCUMENT_ROOT */
-		$webServer = Eresus_WebServer::getInstance();
-		$documentRoot = new ReflectionProperty('Eresus_WebServer', 'documentRoot');
-		$documentRoot->setAccessible(true);
-		$documentRoot->setValue($webServer, '/home/user/public_html');
-
-		$cms = $this->getMock('Eresus_CMS', array('getFsRoot'));
-		$cms->expects($this->any())->method('getFsRoot')
-			->will($this->returnValue('/home/user/public_html/example.org'));
-
-		$request = $this->getMock('UniversalStub', array('setLocalRoot'));
-		$request->expects($this->once())->method('setLocalRoot')->with('/example.org');
-		Eresus_Kernel::sc()->set('request', $request);
-
-		$detectWebRoot = new ReflectionMethod('Eresus_CMS', 'detectWebRoot');
-		$detectWebRoot->setAccessible(true);
-		$detectWebRoot->invoke($cms);
-	}
-
-	/**
-	 * @covers Eresus_CMS::detectWebRoot
-	 */
-	public function test_detectWebRoot_windows()
-	{
-		/* Подменяем DOCUMENT_ROOT */
-		$webServer = Eresus_WebServer::getInstance();
-		$documentRoot = new ReflectionProperty('Eresus_WebServer', 'documentRoot');
-		$documentRoot->setAccessible(true);
-		$documentRoot->setValue($webServer, 'C:/Program Files/Apache Webserver/docs');
-
-		$cms = $this->getMock('Eresus_CMS', array('getFsRoot'));
-		$cms->expects($this->any())->method('getFsRoot')
-			->will($this->returnValue('C:/Program Files/Apache Webserver/docs/example.org'));
-
-		$request = $this->getMock('UniversalStub', array('setLocalRoot'));
-		$request->expects($this->once())->method('setLocalRoot')->with('/example.org');
-		Eresus_Kernel::sc()->set('request', $request);
-
-		$detectWebRoot = new ReflectionMethod('Eresus_CMS', 'detectWebRoot');
-		$detectWebRoot->setAccessible(true);
-		$detectWebRoot->invoke($cms);
-	}
-
-	/**
 	 * @covers Eresus_CMS::getPage
 	 */
-	public function test__getPage()
+	public function test_getPage()
 	{	
 		$p_page = new ReflectionProperty("Eresus_CMS", "page");
 		$p_page->setAccessible(true);
