4b203b42b5594ec00105088b31940ba61f076990
Михаил Красильников <mihalych@vsepofigu.ru>
2012-09-16 19:52:28 +0400

    Исключён кэш основных классов.

--- src/app_dev.php
+++ src/app_dev.php
@@ -2,7 +2,8 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__ . '/../app/autoload.php';
+
 require_once __DIR__ . '/core/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
