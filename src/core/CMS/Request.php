7eecb087a960fb504174451f1dacfb5e0e9d1c78
Михаил Красильников <mihalych@vsepofigu.ru>
2013-07-24 19:07:53 +0400

    Добавлен класс для работы с запросом HTTP на уровне CMS.

---
+++ src/core/CMS/Request.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Запрос к CMS
+ *
+ * @version ${product.version}
+ * @copyright ${product.copyright}
+ * @license ${license.uri} ${license.name}
+ * @author Михаил Красильников <m.krasilnikov@yandex.ru>
+ *
+ * Данная программа является свободным программным обеспечением. Вы
+ * вправе распространять ее и/или модифицировать в соответствии с
+ * условиями версии 3 либо (по вашему выбору) с условиями более поздней
+ * версии Стандартной Общественной Лицензии GNU, опубликованной Free
+ * Software Foundation.
+ *
+ * Мы распространяем эту программу в надежде на то, что она будет вам
+ * полезной, однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том
+ * числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и ПРИГОДНОСТИ ДЛЯ
+ * ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ. Для получения более подробной
+ * информации ознакомьтесь со Стандартной Общественной Лицензией GNU.
+ *
+ * Вы должны были получить копию Стандартной Общественной Лицензии
+ * GNU с этой программой. Если Вы ее не получили, смотрите документ на
+ * <http://www.gnu.org/licenses/>
+ *
+ * @package Eresus
+ */
+
+/**
+ * Запрос к CMS
+ *
+ * Это надстройка над {@link Eresus_HTTP_Request}, адаптирующая запрос HTTP с учётом свойств сайта.
+ *
+ * @package Eresus
+ * @since 3.01
+ */
+class Eresus_CMS_Request extends Eresus_HTTP_Request
+{
+    /**
+     * Корневой адрес сайта
+     *
+     * @var null|string
+     * @since 3.01
+     */
+    private $siteRoot = null;
+
+    /**
+     * Задаёт корневой адрес сайта
+     *
+     * Этот адрес будет вырезаться в таких методах как {@link getPath()}, {@link getDirectry()},
+     * {@link getFile()}.
+     *
+     * @param string $url  корневой URL или только путь относительно корня домена
+     *
+     * @since 3.01
+     */
+    public function setSiteRoot($url)
+    {
+        $this->siteRoot = rtrim(parse_url($url, PHP_URL_PATH), '/');
+        if (substr($this->siteRoot, 0, 1) != '/')
+        {
+            $this->siteRoot = '/' . $this->siteRoot;
+        }
+    }
+
+    /**
+     * Возвращает схему (протокол)
+     *
+     * Если схема не указана, считается, что это «http».
+     *
+     * @return string  «http» или «https»
+     *
+     * @since 3.01
+     */
+    public function getScheme()
+    {
+        $scheme = parent::getScheme();
+        if ('' == $scheme)
+        {
+            $scheme = 'http';
+        }
+        return $scheme;
+    }
+
+    /**
+     * Возвращает хост
+     *
+     * Если хост не задан, считается что это «localhost».
+     *
+     * @return string
+     * @since 3.01
+     */
+    public function getHost()
+    {
+        $host = parent::getHost();
+        if ('' == $host)
+        {
+            $host = 'localhost';
+        }
+        return $host;
+    }
+
+    /**
+     * Возвращает путь (папку и имя файла) из запроса
+     *
+     * @return string
+     * @since 3.01
+     */
+    public function getPath()
+    {
+        $path = parent::getPath();
+        if ('' == $path)
+        {
+            $path = '/';
+        }
+        $path = substr($path, strlen($this->siteRoot));
+        return $path;
+    }
+}
+
---
+++ tests/phpunit/core/CMS/RequestTest.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Тесты класса Eresus_CMS_Request
+ *
+ * @version ${product.version}
+ * @copyright ${product.copyright}
+ * @license ${license.uri} ${license.name}
+ * @author Михаил Красильников <m.krasilnikov@yandex.ru>
+ *
+ * Данная программа является свободным программным обеспечением. Вы
+ * вправе распространять ее и/или модифицировать в соответствии с
+ * условиями версии 3 либо (по вашему выбору) с условиями более поздней
+ * версии Стандартной Общественной Лицензии GNU, опубликованной Free
+ * Software Foundation.
+ *
+ * Мы распространяем эту программу в надежде на то, что она будет вам
+ * полезной, однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том
+ * числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и ПРИГОДНОСТИ ДЛЯ
+ * ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ. Для получения более подробной
+ * информации ознакомьтесь со Стандартной Общественной Лицензией GNU.
+ *
+ * Вы должны были получить копию Стандартной Общественной Лицензии
+ * GNU с этой программой. Если Вы ее не получили, смотрите документ на
+ * <http://www.gnu.org/licenses/>
+ *
+ * @package Eresus
+ * @subpackage Tests
+ */
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+/**
+ * Тесты класса Eresus_CMS_Request
+ * @package Eresus
+ * @subpackage Tests
+ */
+class Eresus_CMS_RequestTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Eresus_CMS_Request::getScheme
+     * @covers Eresus_CMS_Request::setScheme
+     */
+    public function testScheme()
+    {
+        $request = new Eresus_CMS_Request();
+        $this->assertEquals('http', $request->getScheme());
+    }
+
+    /**
+     * @covers Eresus_CMS_Request::getMethod
+     * @covers Eresus_CMS_Request::setMethod
+     */
+    public function testMethod()
+    {
+        $request = new Eresus_CMS_Request();
+        $this->assertEquals('GET', $request->getMethod());
+    }
+
+    /**
+     * @covers Eresus_CMS_Request::getHost
+     * @covers Eresus_CMS_Request::setHost
+     */
+    public function testHost()
+    {
+        $request = new Eresus_CMS_Request();
+        $this->assertEquals('localhost', $request->getHost());
+    }
+
+    /**
+     * @covers Eresus_CMS_Request::getPath
+     * @covers Eresus_CMS_Request::setPath
+     * @covers Eresus_CMS_Request::setSiteRoot
+     */
+    public function testPath()
+    {
+        $request = new Eresus_CMS_Request();
+        $this->assertEquals('/', $request->getPath());
+
+        $request = new Eresus_CMS_Request('http://example.org/site/path/file.php');
+        $this->assertEquals('/site/path/file.php', $request->getPath());
+        $request->setSiteRoot('http://example.org/site/');
+        $this->assertEquals('/path/file.php', $request->getPath());
+        $request->setSiteRoot('site');
+        $this->assertEquals('/path/file.php', $request->getPath());
+    }
+
+    /**
+     * @covers Eresus_CMS_Request::getDirectory
+     * @covers Eresus_CMS_Request::getFile
+     */
+    public function testDirectoryFile()
+    {
+        $request = new Eresus_CMS_Request();
+
+        $this->assertEquals('', $request->getDirectory());
+        $this->assertEquals('', $request->getFile());
+
+        $request = new Eresus_CMS_Request('http://example.org/site/path/file.php');
+        $request->setSiteRoot('http://example.org/site/');
+        $this->assertEquals('/path', $request->getDirectory());
+        $this->assertEquals('file.php', $request->getFile());
+    }
+
+    /**
+     * Формирование URL
+     *
+     * @covers Eresus_CMS_Request::__toString
+     */
+    public function testToString()
+    {
+        $request = new Eresus_CMS_Request();
+        $this->assertEquals('http://localhost/', strval($request));
+    }
+}
+
