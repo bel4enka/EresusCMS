02fd11cd75282c2828f855df49df287a3e1c1dd5
Михаил Красильников <mihalych@vsepofigu.ru>
2013-07-28 10:30:26 +0400

    Удалена функция ecStripSlashes

--- src/core/framework/core/kernel.php
+++ src/core/framework/core/kernel.php
@@ -8,32 +8,3 @@ function eresus_log()
     call_user_func_array(array('Eresus_Kernel', 'log'), func_get_args());
 }
 
-/*****************************************************************************
- *
- *   Functions
- *
- *****************************************************************************/
-
-/**
- * Recursive slash stripping
- *
- * @param string|array $source
- * @return string|array
- *
- * @author Ghost
- */
-function ecStripSlashes($source)
-{
-    if (is_array($source)) {
-
-        foreach ($source as $key => $value) {
-            $source[$key] = ecStripSlashes($source[$key]);
-        }
-
-    } else {
-
-        $source = stripslashes($source);
-    }
-
-    return $source;
-}
