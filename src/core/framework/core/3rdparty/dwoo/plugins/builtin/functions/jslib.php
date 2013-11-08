b7c276b96c2e44058cf444a0f6c0da817c5190dc
Михаил Красильников <m.krasilnikov@yandex.ru>
2013-11-08 09:54:12 +0400

    Форматирование.

--- src/core/framework/core/3rdparty/dwoo/plugins/builtin/functions/jslib.php
+++ src/core/framework/core/3rdparty/dwoo/plugins/builtin/functions/jslib.php
@@ -8,13 +8,13 @@
 /**
  * Подключает библиотеку JavaScript
  *
- * @see WebPage::linkJsLib()
+ * @see Eresus\Templating\Page::linkJsLib()
  * @since 2.16
  */
 function Dwoo_Plugin_jslib(Dwoo $dwoo, $name)
 {
-	$args = func_get_args();
-	array_shift($args);
-	call_user_func_array(array($GLOBALS['page'], 'linkJsLib'), $args);
-	return '';
+    $args = func_get_args();
+    array_shift($args);
+    call_user_func_array(array($GLOBALS['page'], 'linkJsLib'), $args);
+    return '';
 }
