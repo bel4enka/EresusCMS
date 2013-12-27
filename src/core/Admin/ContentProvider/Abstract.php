73d63ca7b22d67d7c71cf5883fca5f05eb77b0e7
Михаил Красильников <m.krasilnikov@yandex.ru>
2013-12-27 15:17:59 +0400

    #26: Абстрактный поставщик контента для АИ

---
+++ src/core/Admin/ContentProvider/Abstract.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Абстрактный поставщик контента для АИ
+ *
+ * @version ${product.version}
+ * @copyright ${product.copyright}
+ * @license ${license.uri} ${license.name}
+ * @author Михаил Красильников <m.krasilnikov@yandex.ru>
+ *
+ * Данная программа является свободным программным обеспечением. Вы
+ * вправе распространять ее и/или модифицировать в соответствии с
+ * условиями версии 3 либо (по вашему выбору) с условиями более поздней
+ * версии Стандартной Общественной Лицензии GNU, опубликованной Free
+ * Software Foundation.
+ *
+ * Мы распространяем эту программу в надежде на то, что она будет вам
+ * полезной, однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том
+ * числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и ПРИГОДНОСТИ ДЛЯ
+ * ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ. Для получения более подробной
+ * информации ознакомьтесь со Стандартной Общественной Лицензией GNU.
+ *
+ * Вы должны были получить копию Стандартной Общественной Лицензии
+ * GNU с этой программой. Если Вы ее не получили, смотрите документ на
+ * <http://www.gnu.org/licenses/>
+ *
+ * @package Eresus
+ */
+
+/**
+ * Абстрактный поставщик контента для АИ
+ *
+ * @package Eresus
+ * @since 3.01
+ */
+abstract class Eresus_Admin_ContentProvider_Abstract
+{
+    /**
+     * Модуль CMS
+     *
+     * @var object
+     *
+     * @since 3.01
+     */
+    protected $module;
+
+    /**
+     * Отрисовывает интерфейс модуля
+     *
+     * @throws LogicException
+     * @throws RuntimeException
+     *
+     * @return string  HTML
+     *
+     * @since 3.01
+     */
+    public function adminRender()
+    {
+        if (!method_exists($this->getModule(), 'adminRender'))
+        {
+            throw new LogicException(sprintf(_('Метод "%s" не найден в классе "%s".'),
+                'adminRender', get_class($this->getModule())));
+        }
+        try
+        {
+            $html = $this->getModule()->adminRender();
+        }
+        catch (Exception $e)
+        {
+            throw new RuntimeException(
+                sprintf(_('В модуле %s произошла ошибка: %s'), $this->getModuleName(),
+                    $e->getMessage(), 0, $e));
+        }
+        return $html;
+    }
+
+    /**
+     * Возвращает модуль-поставщик
+     *
+     * @return object
+     *
+     * @since 3.01
+     */
+    public function getModule()
+    {
+        return $this->module;
+    }
+
+    /**
+     * Возвращает имя модуля, пригодное для вывода пользователю
+     *
+     * @return string
+     *
+     * @since 3.01
+     */
+    abstract public function getModuleName();
+}
+
---
+++ tests/phpunit/core/Admin/ContentProvider/AbstractTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Тесты класса Eresus_Admin_ContentProvider_Abstract
+ *
+ * @version ${product.version}
+ * @copyright ${product.copyright}
+ * @license ${license.uri} ${license.name}
+ * @author Михаил Красильников <m.krasilnikov@yandex.ru>
+ *
+ * Данная программа является свободным программным обеспечением. Вы
+ * вправе распространять ее и/или модифицировать в соответствии с
+ * условиями версии 3 либо (по вашему выбору) с условиями более поздней
+ * версии Стандартной Общественной Лицензии GNU, опубликованной Free
+ * Software Foundation.
+ *
+ * Мы распространяем эту программу в надежде на то, что она будет вам
+ * полезной, однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том
+ * числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и ПРИГОДНОСТИ ДЛЯ
+ * ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ. Для получения более подробной
+ * информации ознакомьтесь со Стандартной Общественной Лицензией GNU.
+ *
+ * Вы должны были получить копию Стандартной Общественной Лицензии
+ * GNU с этой программой. Если Вы ее не получили, смотрите документ на
+ * <http://www.gnu.org/licenses/>
+ *
+ * @package Eresus
+ * @subpackage Tests
+ */
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+/**
+ * Тесты класса Eresus_Admin_ContentProvider_Abstract
+ *
+ * @package Eresus
+ * @subpackage Tests
+ */
+class Eresus_Admin_ContentProvider_AbstractTest extends Eresus_TestCase
+{
+    /**
+     * @covers Eresus_Admin_ContentProvider_Abstract::adminRender
+     * @expectedException LogicException
+     */
+    public function testAdminRenderNoMethod()
+    {
+        $provider = $this->getMockBuilder('Eresus_Admin_ContentProvider_Abstract')
+            ->setMethods(array('getModule', 'getModuleName'))->getMock();
+        $provider->expects($this->any())->method('getModule')
+            ->will($this->returnValue(new stdClass()));
+        /** @var Eresus_Admin_ContentProvider_Abstract $provider */
+        $provider->adminRender();
+    }
+
+    /**
+     * @covers Eresus_Admin_ContentProvider_Abstract::adminRender
+     * @expectedException RuntimeException
+     */
+    public function testAdminRenderException()
+    {
+        $module = $this->getMock('stdClass', array('adminRender'));
+        $module->expects($this->once())->method('adminRender')
+            ->will($this->throwException(new RuntimeException('Bar baz')));
+        $provider = $this->getMockBuilder('Eresus_Admin_ContentProvider_Abstract')
+            ->setMethods(array('getModule', 'getModuleName'))->getMock();
+        $provider->expects($this->any())->method('getModuleName')
+            ->will($this->returnValue('Foo'));
+        $provider->expects($this->any())->method('getModule')
+            ->will($this->returnValue($module));
+        /** @var Eresus_Admin_ContentProvider_Abstract $provider */
+        $provider->adminRender();
+    }
+}
+
